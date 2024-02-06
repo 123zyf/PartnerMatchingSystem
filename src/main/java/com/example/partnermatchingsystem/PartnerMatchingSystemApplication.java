47dcf84b2751cf8c8014160b6a56efe5cabee903
kelion <1060566305@qq.com>
2024-02-06 21:23:19 +0800

    init project

---
+++ src/main/java/com/example/partnermatchingsystem/PartnerMatchingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.partnermatchingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartnerMatchingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartnerMatchingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/partnermatchingsystem/PartnerMatchingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.partnermatchingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartnerMatchingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
